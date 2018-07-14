69412059ea7ae4772eef316f67cb50fd252d8991
Sirui Wang <sirwa@microsoft.com>
2018-07-14 17:05:59 +0800

    using relative path instead of absolution path, separate path to a different file

--- SuperPerfmonAnalyzer/PythonApplication1/Parser.py
+++ SuperPerfmonAnalyzer/PythonApplication1/Parser.py
@@ -1,11 +1,20 @@
 import subprocess
 import sys
 import datetime
+import os
+from inspect import getsourcefile
+from myClass import PerfMon, CounterGroup, Counter
+
+
 
 def convertBlgToCsv(path):
     result = []
-    cmd = "relog " + path.encode('unicode_escape').decode() + " -f csv -o F:\\source\\repos\\SuperPerfmonAnalyzer\\csvrepo\\" + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".csv"
+
+    filename = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".csv"
+
+    cmd = "relog " + path.encode('unicode_escape').decode() + " -f csv -o " + abspath(getsourcefile(lambda:0)) +"csvrepo\\" + filename
     print(cmd)
+
     process = subprocess.Popen(cmd,
                                shell=True,
                                stdout=subprocess.PIPE,
@@ -20,56 +29,5 @@ def convertBlgToCsv(path):
 
 
 
-class PerfMon(object):
-    
-    def __init__(self, name, start_time, end_time, timezone):
-        self.name = name
-        self.start_time = start_time
-        self.end_time = end_time
-        self.timezone = timezone
-
-    def getName(self):
-        return self.name
-
-    def getStartTime(self):
-        return self.start_time
-
-    def getEndTime(self):
-        return self.end_time
-
-    def getTimezone(self):
-        return self.timezone
-    
-    def __str__(self):
-        return "%s is captured in %s" % (self.name, self.timezone)
-
-class CounterGroup(PerfMon):
-    
-    def __init__(self, name, start_time, end_time, timezone, group_name):
-        PerfMon.__init__(self, name, start_time, end_time, timezone)
-        self.group_name = group_name
-
-    def getGroupName(self):
-        return self.group_name
-
-class Counter(CounterGroup):
-
-    stats = []
-
-    def __init__(self, name, start_time, end_time, timezone, group_name, counter_name, parent, instance, computer):
-        CounterGroup.__init__(self, name, start_time, end_time, timezone, group_name)
-        self.counter_name = count_name
-        self.parent = parent
-        self.instance = instance
-        self.computer = computer
-        
-    def getCounterName(self):
-        return self.counter_name
-
-    def addStats(time,value):
-        stats.append((time,value))
-        
-    def showGraph(self):
-        return null
 
 
