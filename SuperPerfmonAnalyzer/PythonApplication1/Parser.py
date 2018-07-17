60f72ec00afa41156fa17210c66678dcbac2b900
sirwa266 <sirwa@microsoft.com>
2018-07-17 19:50:39 +0800

    change normal file path

--- SuperPerfmonAnalyzer/PythonApplication1/Parser.py
+++ SuperPerfmonAnalyzer/PythonApplication1/Parser.py
@@ -13,7 +13,8 @@ def convertBlgToCsv(path):
     filename = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".csv"
 
 #    cmd = "relog " + path.encode('unicode_escape').decode() + " -f csv -o " + abspath(getsourcefile(lambda:0)) +"csvrepo\\" + filename
-    cmd = "relog " + path + " -f csv -o " + os.path.abspath(getsourcefile(lambda:0)) +"\\csvrepo\\" + filename
+    myDirname = os.path.normpath(path)
+    cmd = "relog " + myDirname + " -f csv -o " + os.path.abspath(getsourcefile(lambda:0)) +"\\csvrepo\\" + filename
 
     print(cmd)
 
